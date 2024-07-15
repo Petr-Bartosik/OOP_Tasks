97dcf283d183743c9a20778567d5472d2db7c7e6
Petr-Bartosik <petr.bartosik@seznam.cz>
2024-07-15 18:53:11 +0200

    Tasky

---
+++ human.py
@@ -0,0 +1,16 @@
+class Human:
+    name = ("Adam")
+    age = 23
+
+    def walk(self):
+        print(f""Im walking")
+
+
+h=Human()
+
+print(h)
+print(h.name)
+h.name = "Eva"
+
+print (h.name)
+print(f"h2.name: {h.name}")
